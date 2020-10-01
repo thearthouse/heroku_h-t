eb3676cc6cb3c11229b44d0d7ac17fbdd0666800
thearthouse <realist.tm@yandex.com>
2020-10-01 23:44:17 +0300

    Update visit.php

--- visit.php
+++ visit.php
@@ -7,14 +7,16 @@ function file_getcontent_with_proxy($urltoget) {
     $url = $urltoget;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,$url);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_HEADER, 1);
     curl_setopt($ch, CURLOPT_TIMEOUT, 30);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // read more about HTTPS 
     curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Linux; Android 10; IN2011 Build/QKQ1.191222.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/84.0.4147.89 Mobile Safari/537.36 GSA/11.20.15.21.arm64');
     $curl_scraped_page = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
+    file_put_contents("php://stderr", "$httpcode $url\n");
     return $curl_scraped_page;
 }
 
